f8a02dcea4a1b426f22a5a37e4c3ca5f4c34ab70
Yasminabh <167634897+Yasminabh@users.noreply.github.com>
2024-04-24 18:12:19 +0200

    Calculadora.java

--- Calculadora/src/main/java/com/daw/calculadora/Calculadora.java
+++ Calculadora/src/main/java/com/daw/calculadora/Calculadora.java
@@ -1,16 +1,2 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
-package com.daw.calculadora;
-
-/**
- *
- * @author Usuario
- */
-public class Calculadora {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
+// desde netbeans
+//comentario desde github;)
